12ba4ce2ce8249d6f5b37de4a8d422ba012c2106
lucianasfoliveira <luluzinhasfo@gmail.com>
2023-05-08 17:36:53 -0300

    Commit inicial

---
+++ ProjetoGestaoRH/src/main/java/br/com/taking/ProjetoGestaoRH/repository/CandidatoRepository.java
@@ -0,0 +1,24 @@
+package br.com.taking.ProjetoGestaoRH.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.taking.ProjetoGestaoRH.model.Candidato;
+
+
+public interface CandidatoRepository extends JpaRepository<Candidato, Integer>{
+	
+	
+	// Nome da entidade e atributo
+	@Query("select c from Candidato c where c.identificationDocument= :identificationDocument" )
+	List<Candidato> findAll(String identificationDocument);
+	
+
+	
+	
+}
+	
+	
+	
